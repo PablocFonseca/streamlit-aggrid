dfb184b01bef5348091efbe1f7739e7cff5aa7d0
pablocfonseca <pablofonseca@gmail.com>
2021-01-06 18:39:23 -0300

    - removed simplejson import

--- st_aggrid/__init__.py
+++ st_aggrid/__init__.py
@@ -2,7 +2,7 @@ import os
 import streamlit.components.v1 as components
 import pandas as pd
 import numpy as np
-import simplejson as json
+import json
 
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 from st_aggrid.shared import GridUpdateMode, DataReturnMode, JsCode, walk_gridOptions

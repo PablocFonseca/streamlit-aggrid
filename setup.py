42e42de426c699a2a99682820f25bd3173ff68ce
pablocfonseca <pablofonseca@gmail.com>
2021-08-02 11:26:21 -0300

    upgraded ag-grid to 25.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="streamlit-aggrid",
-    version="0.2.0",
+    version="0.2.1",
     author="Pablo Fonseca",
     author_email="pablo.fonseca+pip@gmail.com",
     description="Streamlit component implementation of ag-grid",

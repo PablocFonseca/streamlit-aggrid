34573dcf7e0bf2f3827800f710395da4e321d785
Pablo Fonseca <pablofonseca@gmail.com>
2022-07-04 21:25:12 -0300

    updated grid redraw example

--- examples/example.py
+++ examples/example.py
@@ -7,7 +7,7 @@ root = os.path.join(os.path.dirname(__file__))
 
 dashboards = {
     "Main Example": os.path.join(root, "main_example.py"),
-    "Fixed Key Parameter": os.path.join(root, "fixed_key_example.py"),
+    "Controling AgGrid redraw": os.path.join(root, "fixed_key_example.py"),
     "License Key": os.path.join(root, "licensing_example.py"),
     "Two grids in page": os.path.join(root, "two_grids_example.py"),
     "Virtual Columns": os.path.join(root, "virtual_columns.py"),
